941297f94f2129e7f7611bff8047dc08d8b110e3
abhishekMukesh <abhishekMukesh@users.noreply.github.com>
2016-02-01 19:57:16 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions below create a list of functions over a matrix and then caches the inverse globally for this object which gets retrived if
+## if called cachesolve is called on same object twice, possible way to test :
+## L<-makeCacheMatrix(matrix(c(1,2,3,0,1,4,5,6,0),3,3,TRUE))
+## cacheSolve(L)   #Should give the inverse matrix
+## cacheSolve(L)   #Should display message "getting cached data" then display the inverse
 
-## Write a short comment describing this function
+##makeCacheMatrix builds a list of pointers to functions to its argument(x) 
+##in the current scenario the set function is not used, however it can be used to reset x from within cachesolve using the ... argument
+##get function returns the value of x from the scope of this function
+##setmatrix sets m to its argument globally
+##getmatrix returns the value of m (which is in global scope due to setmatrix)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+m<-NULL
+  set<-function(y){
+  x<<-y
+  m<<-NULL
+}
+get<-function() x
+setmatrix<-function(solve) m<<- solve
+getmatrix<-function() m
+list(set=set, get=get,
+   setmatrix=setmatrix,
+   getmatrix=getmatrix)
 }
 
 
-## Write a short comment describing this function
+## This function takes as input a list of functions from the previous function makeCacheMatrix called on a matrix,
+##if m is previously set using this function then it simply returns the value from context else it computes and sets m to the inverse
+##inside the object created by makeCacheMatrix, thus calling it repeatedly on same list causes cached data to be read.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m<-x$getmatrix()
+    if(!is.null(m)){
+      message("getting cached data")
+      return(m)
+    }
+    matrix<-x$get()
+    m<-solve(matrix, ...)
+    x$setmatrix(m)
+    m
 }
